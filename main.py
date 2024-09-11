7381553d767000eea1789266fff56d3048bb8587
Martin Cooley <martin.cooley@protonmail.com>
2024-09-11 10:18:25 -0400

    wip

--- main.py
+++ main.py
@@ -195,7 +195,7 @@ if __name__ == "__main__":
         half = numEmails/2
         once = False
         for x, email in enumerate(emails):
-            if email == os.environ.get('badmentor1') or email == os.environ.get('badmentor2'):
+            if email == os.environ.get('badmentor1') or email == os.environ.get('badmentor2') or email == os.environ.get('badmentor3'):
                 continue
             if x >= half and once is False:
                 for _ in range(0, 5):
