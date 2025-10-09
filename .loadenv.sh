bfe0c488e15eceebcf55289ceaa20f8961997aa3
Martin Cooley <martin.cooley@protonmail.com>
2025-10-09 14:59:10 -0400

    Remove .loadenv.sh

--- .loadenv.sh
+++
@@ -1,6 +0,0 @@
-export mslUsername=mcooley
-export mslPassword=aq1
-export db_url=postgresql://martin-refmentor-ui-support:XPjN0K8v8ZwTiLPo9PXgWQ@refmentor-roach-2992.g8z.cockroachlabs.cloud:26257/defaultdb?sslmode=verify-full
-export db_url_local=postgresql://postgres:docker@localhost:5432
-export badmentor1=diane.florkowski@gmail.com
-export badmentor2=chuckor@aol.com
