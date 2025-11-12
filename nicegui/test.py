b07f440d9d176505ae2578f03c63ee0d6eed5b95
Martin Cooley <martin.cooley@protonmail.com>
2025-11-11 20:14:46 -0500

    remove nicegui attempt

--- nicegui/test.py
+++
@@ -1,7 +0,0 @@
-from nicegui import ui
-
-ui.textarea(label='Text', placeholder='start typing',
-            on_change=lambda e: result.set_text('you typed: ' + e.value))
-result = ui.label()
-
-ui.run()
